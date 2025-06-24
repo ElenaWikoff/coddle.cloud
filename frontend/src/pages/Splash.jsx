097d31ae7441a1bc20abdb0169954ffd00a4fae9
ElenaWikoff <elenawikoff@gmail.com>
2025-06-24 13:29:35 -0500

    Test frontend/backend communication

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 
 const Splash = () => {
+    const [text, setText] = useState("not received");
+
+    useEffect(() => {
+        fetch('http://localhost:8080/frontendhitme')
+        .then((res) => res.text())
+        .then((text) => setText(text));
+    }, []);
+
     return (
         <main>
-            <p>Splash Page</p>
+            <p>{`${text}`}</p>
         </main>
     );
 };
