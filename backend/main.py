4d02a7096a5359743d818bde37f63dc1f1e6d4de
Ethan Do <ethando767243@gmail.com>
2025-06-24 21:00:35 -0500

    filter_members_json

--- backend/main.py
+++ backend/main.py
@@ -18,9 +18,33 @@ def index():
 
 @app.route('/about')
 def aboutIndex():
+    excluded_names = {"Kevin Xu", "cs373-idb-gitlab-rest"}
+
     members = gl.projects.get(PROJECT_ID).members.list(get_all=True)
-    members_json = [member.attributes for member in members]
+    members_json = [
+        {
+            "name": member.attributes.get("name"),
+            "username": member.attributes.get("username"),
+            "web_url": member.attributes.get("web_url")
+        }
+        for member in members
+        if member.attributes.get("name") not in excluded_names
+    ]
     return members_json
 
 if __name__ == "__main__":
+    # testing filtered output below
+    # excluded_names = {"Kevin Xu", "cs373-idb-gitlab-rest"}
+
+    # members = gl.projects.get(PROJECT_ID).members.list(get_all=True)
+    # members_json = [
+    #     {
+    #         "name": member.attributes.get("name"),
+    #         "username": member.attributes.get("username"),
+    #         "web_url": member.attributes.get("web_url")
+    #     }
+    #     for member in members
+    #     if member.attributes.get("name") not in excluded_names
+    # ]
+    # print(members_json)
     app.run(host='0.0.0.0', port=8080)
