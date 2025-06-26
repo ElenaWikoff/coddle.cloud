b96de06a09dff10c83e2e8320124709e21f72c8e
ElenaWikoff <elenawikoff@gmail.com>
2025-06-26 17:58:37 -0500

    Add info to lure pages

--- frontend/src/pages/Lure.jsx
+++ frontend/src/pages/Lure.jsx
@@ -1,13 +1,73 @@
-import { useParams } from "react-router";
+import { Link, useLoaderData, useParams, useNavigate } from "react-router";
+import Container from "react-bootstrap/esm/Container";
+import ListGroup from "react-bootstrap/esm/ListGroup";
 import PageContainer from "../components/PageContainer";
+import { BsArrowLeft } from "react-icons/bs";
+import { capitalizeEachWord } from "../utils/functions.jsx";
+import { useEffect, useState } from "react";
 
 const Lure = () => {
-    const {id} = useParams();
-    return (
-        <PageContainer>
-            Lure Page, id {id}
-        </PageContainer>
-    )
+   const navigate = useNavigate();
+   const data = useLoaderData();
+
+   return (
+      <PageContainer>
+         {data && (
+            <Container className="m-4 g-4">
+               <a className="cta" onClick={() => navigate(-1)}>
+                  <BsArrowLeft /> Go Back
+               </a>
+
+               <h2 className="page-title mt-4">
+                  {capitalizeEachWord(data.name)}
+               </h2>
+
+               <h3
+                  className="page-subtitle"
+                  style={{ fontWeight: "400", fontSize: "1.5em" }}
+               >
+                  {capitalizeEachWord(data.brand)}
+               </h3>
+
+               <ListGroup>
+                  <ListGroup.Item>
+                     Type: {capitalizeEachWord(data.type)}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                     Application:&nbsp;
+                     {data.application.map((item, index, array) => {
+                           return (
+                              <span
+                                 key={`item-${index}`}
+                              >
+                                 {`
+                                    ${capitalizeEachWord(item)}${
+                                    index !== array.length - 1 ? ", " : ""
+                                 }`}
+                              </span>
+                           );
+                        })}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                     Good For:&nbsp;
+                     {data.fish_types.map((item, index, array) => {
+                           return (
+                              <span
+                                 key={`item-${index}`}
+                              >
+                                 {`
+                                    ${capitalizeEachWord(item)}${
+                                    index !== array.length - 1 ? ", " : ""
+                                 }`}
+                              </span>
+                           );
+                        })}
+                  </ListGroup.Item>
+               </ListGroup>
+            </Container>
+         )}
+      </PageContainer>
+   );
 };
 
 export default Lure;
--- frontend/src/pages/Lures.jsx
+++ frontend/src/pages/Lures.jsx
@@ -3,13 +3,14 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/esm/Container";
 import PageContainer from "../components/PageContainer";
+import { capitalizeEachWord } from "../utils/functions";
 
 const Lures = () => {
-    const { results, pagination } = useLoaderData();
+   const { results, pagination } = useLoaderData();
 
-    return (
-        <PageContainer>
-            <Container className="mt-4">
+   return (
+      <PageContainer>
+         <Container className="mt-4">
             <h1>Lures</h1>
             <ListGroup>
                {results &&
@@ -21,18 +22,16 @@ const Lures = () => {
                            as={Link}
                            to={`/lures/${lure.id}`}
                         >
-                           <Col xs="auto">{lure.id}</Col>
-                           <Col>{lure.type}</Col>
-                           <Col>{lure.brand}</Col>
-                           <Col>{lure.application}</Col>
-                           <Col>{lure.fish_types}</Col>
+                           <Col style={{ fontWeight: "600" }}>
+                              {capitalizeEachWord(lure.name)}
+                           </Col>
                         </ListGroup.Item>
                      );
                   })}
             </ListGroup>
          </Container>
-        </PageContainer>
-    );
+      </PageContainer>
+   );
 };
 
 export default Lures;
