8da5fa7de76eb0c20e6fdf563b064c354d8a806c
ElenaWikoff <elenawikoff@gmail.com>
2025-06-30 21:54:30 -0500

    Fix Fish page

--- frontend/src/pages/Fish.jsx
+++ frontend/src/pages/Fish.jsx
@@ -1,11 +1,10 @@
 import { Link, useLoaderData, useNavigate } from "react-router";
 import Container from "react-bootstrap/esm/Container";
 import ListGroup from "react-bootstrap/esm/ListGroup";
-import { capitalizeEachWord } from "../utils/functions.jsx";
+import { capitalizeEachWord, getDistribution } from "../utils/functions.jsx";
 import PageContainer from "../components/PageContainer";
-import Ratio from "react-bootstrap/Ratio";
 import { BsArrowLeft } from "react-icons/bs";
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 
 const Fish = () => {
    const navigate = useNavigate();
@@ -19,17 +18,20 @@ const Fish = () => {
                   <BsArrowLeft /> Go Back
                </a>
                <figure>
-                  <Ratio
-                     style={{ maxWidth: "800px", margin: "0 auto" }}
-                     aspectRatio="16x9"
-                  >
+                  <div className="image-wrapper" style={{maxWidth: '500px'}}>
                      <img
                         style={{ objectFit: "contain" }}
-                        src={`/images/${data.image.filename}`}
+                        src={
+                           data.image_attribution
+                              ? `/images/fish/${data.id}.png`
+                              : `/images/fish/placeholder.png`
+                        }
                         alt={capitalizeEachWord(data.common_name)}
                      />
-                  </Ratio>
-                  {data.image.attribution && <figcaption>{parse(data.image.attribution)}</figcaption>}
+                  </div>
+                  {data.image_attribution && (
+                     <figcaption>{parse(data.image_attribution)}</figcaption>
+                  )}
                </figure>
 
                <h2 className="page-title mt-4">
@@ -40,15 +42,15 @@ const Fish = () => {
                      data.scientific_name.slice(1)}
                </h3>
                <ListGroup>
-                  <ListGroup.Item>Type: {data.type}</ListGroup.Item>
+                  <ListGroup.Item>Type: {capitalizeEachWord(data.type)}</ListGroup.Item>
                   <ListGroup.Item>
-                     Environment: {data.environment}
+                     Environment: {capitalizeEachWord(data.environment)}
                   </ListGroup.Item>
                   <ListGroup.Item>
-                     Distribution: {data.distribution}
+                     Distribution: {getDistribution(data.distribution)}
                   </ListGroup.Item>
-                  <ListGroup.Item>{`Max Length: ${data.max_sizes.length} ${data.max_sizes_units.length}`}</ListGroup.Item>
-                  <ListGroup.Item>{`Max Weight: ${data.max_sizes.weight} ${data.max_sizes_units.weight}`}</ListGroup.Item>
+                  <ListGroup.Item>{`Max Length: ${data.length} cm`}</ListGroup.Item>
+                  <ListGroup.Item>{`Max Weight: ${data.weight} kg`}</ListGroup.Item>
                </ListGroup>
             </Container>
          )}
