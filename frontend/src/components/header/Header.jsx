67f05e1d13ae92a8cb20a641c0a0196bac6274ad
PerryE25 <perryehimuh@gmail.com>
2025-07-22 22:50:23 -0500

    remove fluid from header container that defaults the logo and everything to be left and right aligned so that it's in the middle of header now

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,38 +4,44 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router";
 import { ReactSVG } from "react-svg";
 import logo from "../../assets/coddle-logo-dark.svg";
-// import logoPNG from "../../assets/coddle-logo-dark-16x9.png";
 import "./header.css";
 
 const Header = () => {
-   return (
-      <Navbar expand="lg" variant="dark">
-         <Container fluid className="d-flex justify-content-between align-items-center">
-            <Navbar.Brand as={Link} to="/">
-               <ReactSVG
-               beforeInjection={(svg) => {
-                  svg.classList.add("header-logo");
-                  svg.setAttribute("style", "width: 150px");
-               }}
-               className="logo-wrapper"
-               desc="Coddle.cloud Logo"
-               title="Coddle.cloud"
-               fallback={() => <h1>Coddle.cloud</h1>}
-               src={logo}
-               />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-               <Nav className="d-flex justify-content-between align-items-center gap-4">
-               <Nav.Link as={Link} to="/spots">Fishing Spots</Nav.Link>
-               <Nav.Link as={Link} to="/fish-species?page=1&limit=12">FishDB</Nav.Link>
-               <Nav.Link as={Link} to="/lures?page=1&limit=12">Lure Options</Nav.Link>
-               <Nav.Link as={Link} to="/about">About</Nav.Link>
-               </Nav>
-            </Navbar.Collapse>
-         </Container>
-      </Navbar>
-   );
+  return (
+    <Navbar expand="lg" variant="dark" className="bg-custom-blue">
+      <Container className="header-grid">
+        {/* Grid Column 2 of 6 */}
+        <div className="grid-logo">
+          <Navbar.Brand as={Link} to="/" className="logo-wrapper">
+            <ReactSVG
+              beforeInjection={(svg) => {
+                svg.classList.add("header-logo");
+                svg.setAttribute("style", "width: 150px");
+              }}
+              className="logo-wrapper"
+              desc="Coddle.cloud Logo"
+              title="Coddle.cloud"
+              fallback={() => <h1>Coddle.cloud</h1>}
+              src={logo}
+            />
+          </Navbar.Brand>
+        </div>
+
+        {/* Grid Columns 4–5 of 6 */}
+        <div className="grid-nav">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="nav-links">
+              <Nav.Link as={Link} to="/spots">Fishing Spots</Nav.Link>
+              <Nav.Link as={Link} to="/fish-species?page=1&limit=12">FishDB</Nav.Link>
+              <Nav.Link as={Link} to="/lures?page=1&limit=12">Lure Options</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
