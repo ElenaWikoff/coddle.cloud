b5db5e565ad457e02b1577059fbd09210e28d0eb
PerryE25 <perryehimuh@gmail.com>
2025-07-22 22:36:30 -0500

    justfiy navbar on header space evenly

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -10,35 +10,27 @@ import "./header.css";
 const Header = () => {
    return (
       <Navbar expand="lg" variant="dark">
-         <Container fluid>
+         <Container fluid className="d-flex justify-content-between align-items-center">
             <Navbar.Brand as={Link} to="/">
                <ReactSVG
-                  beforeInjection={(svg) => {
-                     svg.classList.add("header-logo");
-                     svg.setAttribute("style", "width: 150px");
-                  }}
-                  className="logo-wrapper"
-                  desc="Coddle.cloud Logo"
-                  title="Coddle.cloud"
-                  fallback={() => <h1>Coddle.cloud</h1>}
-                  src={logo}
+               beforeInjection={(svg) => {
+                  svg.classList.add("header-logo");
+                  svg.setAttribute("style", "width: 150px");
+               }}
+               className="logo-wrapper"
+               desc="Coddle.cloud Logo"
+               title="Coddle.cloud"
+               fallback={() => <h1>Coddle.cloud</h1>}
+               src={logo}
                />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-               <Nav className="d-flex justify-content-end w-100">
-                  <Nav.Link as={Link} to="/spots">
-                     Fishing Spots
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/fish-species?page=1&limit=12">
-                     FishDB
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/lures?page=1&limit=12">
-                     Lure Options
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/about">
-                     About
-                  </Nav.Link>
+               <Nav className="d-flex justify-content-between align-items-center gap-4">
+               <Nav.Link as={Link} to="/spots">Fishing Spots</Nav.Link>
+               <Nav.Link as={Link} to="/fish-species?page=1&limit=12">FishDB</Nav.Link>
+               <Nav.Link as={Link} to="/lures?page=1&limit=12">Lure Options</Nav.Link>
+               <Nav.Link as={Link} to="/about">About</Nav.Link>
                </Nav>
             </Navbar.Collapse>
          </Container>
